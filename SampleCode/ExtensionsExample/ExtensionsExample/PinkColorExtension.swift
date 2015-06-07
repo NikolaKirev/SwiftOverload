716f02b8cb7264bdd0cc41308a9803b532b1a132
Nikola Kirev <nikola.kirev@gmail.com>
2015-06-07 15:18:05 +0100

    Added Extensions code example

---
+++ SampleCode/ExtensionsExample/ExtensionsExample/PinkColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  PinkColorExtension.swift
+//  ExtensionsExample
+//
+//  Created by Nikola Kirev on 7/6/15.
+//  Copyright (c) 2015 swiftoverload. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func pinkColor() -> UIColor {
+        return UIColor(red: 1.0, green: 0.412, blue: 0.706, alpha: 1.0)
+    }
+}
---
+++ SampleCode/ExtensionsExample/ExtensionsExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExtensionsExample
+//
+//  Created by Nikola Kirev on 7/6/15.
+//  Copyright (c) 2015 swiftoverload. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.pinkColor()
+    }
+}
+
